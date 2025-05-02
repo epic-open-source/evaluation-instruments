7aefe6487357a552b9608beb72ec2e67be2190bb
bwd <bdiehl@epic.com>
2025-05-02 14:27:39 +0000

    📦automate a path to keep keys in secrets & env

--- .devcontainer/image/stagefiles/layer.sh
+++ .devcontainer/image/stagefiles/layer.sh
@@ -26,3 +26,11 @@ apt-get autoremove -y
 apt-get clean
 
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+# Append to seismo user's .bashrc to source secrets/.env if it exists
+echo '
+# Source secrets/.env if it exists
+if [ -f "$HOME/workspace/secrets/.env" ]; then
+    source "$HOME/workspace/secrets/.env"
+fi
+' >> /home/seismo/.bashrc
